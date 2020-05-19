43b153d7a7fbb0f2a50bbb07f609a03b6fb7ef8b
Alexander Nakahara <alexander.nakahara@ludospro.com.br>
2020-05-18 23:05:18 -0300

    Layouts responsivos, correção de menu

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -2,11 +2,6 @@ $(document).ready(function () {
   $('#datetimepicker1').datetimepicker();
 });
 
-console.log('to aqui')
-
-
-
-
 function onSubmitAgendar(){
 	document.getElementById("myForm").submit();
 }
